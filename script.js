6ba08158620656eeaf90296dee9eb02de1e8dace
AmineD1410 <aminedebaya@gmail.com>
2022-09-14 22:19:12 +0100

    added toggle 'read/unread'

--- script.js
+++ script.js
@@ -7,18 +7,18 @@ function Book (book,author,pages,status) {
     this.status = status;
 }
 
-
+let i=0;
 function addBookToLibrary() {
+  i++;
   const book=document.getElementById('book').value;
   const author=document.getElementById('author').value;
   const pages=document.getElementById('pages').value;
   const statusInt=document.getElementById('status').value;
-  let status = '';
+  let status = ''; 
     if(statusInt === 'yes') {status='Read'}
     else if (statusInt === 'no') {status='Not Read'}
     let userObj=new Book (book,author,pages,status);
     myLibrary.push(userObj);
-    console.table(myLibrary);
     const table = document.getElementById('table');
     const tRow = document.createElement('tr');
     table.appendChild(tRow);
@@ -33,10 +33,19 @@ function addBookToLibrary() {
     const pagesS = document.createElement('td');
     tRow.appendChild(pagesS);
     pagesS.textContent = pages;
-
-    const statusS = document.createElement('td');
-    tRow.appendChild(statusS);
+    const statusR = document.createElement('td');
+    const statusS = document.createElement('button');
+    statusS.setAttribute('id', `toggle-${i}`);
+    statusS.setAttribute('class', 'toggle-btn');
+    tRow.appendChild(statusR);
+    statusR.appendChild(statusS);
     statusS.textContent = status;
+    const statusToggle = document.getElementById(`toggle-${i}`);
+    statusToggle.addEventListener('click', () => {
+      if(statusToggle.textContent=='Read') {statusToggle.textContent='Not Read'}
+      else if(statusToggle.textContent=='Not Read') {statusToggle.textContent='Read'}
+    })
 }
+
 const add = document.getElementById('add');
 add.addEventListener('click', addBookToLibrary);
